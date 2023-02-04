e969f045f36bc0716bccbd844f3a3cd8a5e86c2e
James Sample <jamesgsample@gmail.com>
2023-02-04 14:17:25 +0000

    added pagination funcionality and updated readme, now submitting for code review

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,8 @@ import { React, useState } from "react";
 import "../styles/search.css";
 import getImages from "../requests/getImages";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket } from "@fortawesome/free-solid-svg-icons";
 
 const SearchComponent = ({ setSearchResults }) => {
   const [value, setValue] = useState("");
@@ -17,8 +19,10 @@ const SearchComponent = ({ setSearchResults }) => {
           type="text"
           className="search-box"
           onChange={(e) => setValue(e.target.value)}
+          placeholder="Search for something in space.."
         />
         <button type="submit" className="search-button">
+          <FontAwesomeIcon icon={faRocket} />
           Search
         </button>
       </form>
--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,23 @@ import { React, useState } from "react";
 import "../styles/app.css";
 import SearchComponent from "./Search";
 import SearchResults from "./SearchResults";
+import Pagination from "./Pagination";
 
 const App = () => {
   const [searchResults, setSearchResults] = useState([]);
+  // pagination functionality
+  const [currentPage, setCurrentPage] = useState(1);
+  const [imagesPerPage] = useState(12);
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  //images displayed on page
+  const currentImages = searchResults.slice(
+    indexOfFirstImage,
+    indexOfLastImage
+  );
+  //calculate number of pages
+  const nPages = Math.ceil(searchResults.length / imagesPerPage);
+
   return (
     <div className="App">
       <img
@@ -13,7 +27,12 @@ const App = () => {
         alt="nasalogo"
       />
       <SearchComponent setSearchResults={setSearchResults} />
-      <SearchResults results={searchResults} />
+      <SearchResults results={currentImages} />
+      <Pagination
+        nPages={nPages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
